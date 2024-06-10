6dc51d27071d885c5d3d61682bfa0cdb69205fbb
eggei <macitegeercan@gmail.com>
2024-06-10 17:13:31 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ege Ercan on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
